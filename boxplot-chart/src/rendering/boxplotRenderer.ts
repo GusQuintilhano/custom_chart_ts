9e3358dfa5a778e784fc1f463548ee1e60685824
Gustavo Quintilhano <dts.gquintilhano@ifood3rd.com>
2026-01-12 18:58:43 -0300

    Adicionar imports faltantes de valueToYCoordinate e valueToXCoordinate em boxplotRenderer

--- boxplot-chart/src/rendering/boxplotRenderer.ts
+++ boxplot-chart/src/rendering/boxplotRenderer.ts
@@ -14,6 +14,7 @@ import { renderDotPlot } from './dotPlot';
 import { formatValue } from '@shared/utils/formatters';
 import type { GridLinesConfig } from '../types/boxplotTypes';
 import { generateSvgTitle } from '../utils/tooltipUtils';
+import { valueToYCoordinate, valueToXCoordinate } from '../utils/boxplotCalculations';
 
 /**
  * Renderiza linhas de grade de fundo
